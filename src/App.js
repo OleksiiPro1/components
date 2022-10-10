23d039c03ac61325f6fe1740a25a020f63b044f6
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2022-10-10 13:18:40 +0200

    ConditionalRendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ConditionalRendering from './components/ConditionalRendering';
 import MapOverArrays from './components/MapOverArrays';
 import ShowAndDestruct from './components/ShowAndDestruct';
 import UpdatedTitle from './components/UpdatedTitle';
@@ -13,6 +14,10 @@ function App() {
         <ShowAndDestruct />
         <h1>MapOverArrays</h1>
         <MapOverArrays />
+        <h1>ConditionalRendering</h1>
+        <ConditionalRendering />
+        <br />
+        <br />
       </header>
     </div>
   );
--- src/components/ConditionalRendering.js
+++ src/components/ConditionalRendering.js
@@ -1,3 +1,41 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useState } from 'react';
+
+const falseDiv = css`
+  background-color: black;
+  color: white;
+  padding: 30px;
+`;
+const trueDiv = css`
+  background-color: white;
+  color: black;
+  padding: 30px;
+`;
 export default function ConditionalRendering() {
-  return <div></div>;
+  const [condition, setCondition] = useState(true);
+
+  if (condition) {
+    return (
+      <div css={trueDiv}>
+        true
+        <button onClick={() => setCondition(false)}>to false</button>
+      </div>
+    );
+  } else {
+    return (
+      <div css={falseDiv}>
+        false
+        <button onClick={() => setCondition(true)}>to true</button>
+      </div>
+    );
+  }
+  // return (
+  //   <div>
+  //     {condition ? 'true' : 'false'}{' '}
+  //     <button onClick={() => setCondition(!condition)}>
+  //       {condition ? 'false' : 'true'}
+  //     </button>
+  //   </div>
+  // );
 }
